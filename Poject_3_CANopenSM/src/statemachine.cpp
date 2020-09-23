a3f6b4645fa48fe870eb10b3d20e35c4174a7586
sigurbergurm <sigurbergur1@hotmail.com>
2020-09-23 10:34:21 +0000

    Part 1 of project done
    States Initilization and Operational in working order

--- Poject_3_CANopenSM/src/statemachine.cpp
+++ Poject_3_CANopenSM/src/statemachine.cpp
@@ -1,100 +1,90 @@
 #include <statemachine.h>
 
-PWM_out ledTimer;
-int ledIntensity;
+
+
 
 stateMachine::stateMachine(){
     //Initilization
     ledUsed = false;
-    state = 1;      //state = Initilization
-    cli();          //Clear flags by interrupts
+    state = 1; 
+    //Initialising digital and analog pins
+    stateMachine::ledTimer.init();
+    stateMachine::lightInput.init();
 }
 
 stateMachine::stateMachine(bool LED){
 
-    ledTimer.init();        //Initialzing the pwm timers and styff
-    if(LED)
-    {
-        //The LED is .... so we do something other than have the LED fully lit
-        ledUsed = true;
-        testInput.init();
-        blink();
-        analogReadValue = testInput.read();
-        Serial.println(analogReadValue);
-        // if(analogReadValue < 1023/2){
-        //     ledIntensity = 255;
-        // }
-        // else
-        // {
-        //     ledIntensity = 0;
-        // }
-        
+    // Serial.println(stateMachine::lightInput.read());
+    if(LED){
+        //The LED is in use so do something else
+        ledIntensity = map(stateMachine::lightInput.read(),400,860,0,255);
+        stateMachine::ledTimer.set(ledIntensity);
     }
-    else
-    {
-        //The LED is not in use so the default is do have the LED is fully lit
+    else{
         ledUsed = false;
-        ledTimer.set(255);  //Duty cycle 255 meaning it is fully lit
+        stateMachine::ledTimer.set(255);
     }
     
 }
 
-void stateMachine::blink(){
-    // this code sets up timer1 for a 1 s  @ 16Mhz system clock (mode 4)
-    TCCR1A = 0; // set entire TCCR1A register to 0
-    TCCR1B = 0; // same for TCCR1B
-    TCNT1 = 0;  //initialize counter value to 0
+// void stateMachine::blink(){
+//     // this code sets up timer1 for a 1 s  @ 16Mhz system clock (mode 4)
+//     TCCR1A = 0; // set entire TCCR1A register to 0
+//     TCCR1B = 0; // same for TCCR1B
+//     TCNT1 = 0;  //initialize counter value to 0
 
-    OCR1A = 16000000 / 1024 - 1;         // assign target count to compare register A (must be less than 65536)
-    TCCR1B |= (1 << WGM12);              // clear the timer on compare match A (Mode 4, CTC on OCR1A)
-    TIMSK1 |= (1 << OCIE1A);             // set interrupt on compare match A
-    TCCR1B |= (1 << CS12) | (1 << CS10); // set prescaler to 1024 and start the timer
-    sei();  //enable interrupts
-}
+//     OCR1A = 16000000 / 1024 - 1;         // assign target count to compare register A (must be less than 65536)
+//     TCCR1B |= (1 << WGM12);              // clear the timer on compare match A (Mode 4, CTC on OCR1A)
+//     TIMSK1 |= (1 << OCIE1A);             // set interrupt on compare match A
+//     TCCR1B |= (1 << CS12) | (1 << CS10); // set prescaler to 1024 and start the timer
+//     sei();  //enable interrupts
+// }
 
-ISR(TIMER1_COMPA_vect)
+// ISR(TIMER1_COMPA_vect)
+// {
+//   // action to be done every 1 sec
+//     if(ledIntensity == 255){
+//         ledIntensity = 0;
+//         ledTimer.set(ledIntensity);
+//         Serial.println("HI");
+//     }
+//     else if(ledIntensity == 0)
+//     {
+//         ledIntensity = 255;
+//         ledTimer.set(ledIntensity);
+//         Serial.println("BYE");
+//     }
+    
+// }
+
+void stateMachine::updateState(char stepOrBack)
 {
-  // action to be done every 1 sec
-    if(ledIntensity == 255){
-        ledIntensity = 0;
-        ledTimer.set(ledIntensity);
-        Serial.println("HI");
-    }
-    else if(ledIntensity == 0)
-    {
-        ledIntensity = 255;
-        ledTimer.set(ledIntensity);
-        Serial.println("BYE");
-    }
-    
-}
-
-void stateMachine::step(){
     //Goes to next state according to statemachine
-    
+    //According to the char that is sent in
+    // 's' = step, 'b' = back
+
     switch (state)
     {
-    case 1:
-        operational();
-        state = 2;
+    case 1:              //Initilization state
+        if(stepOrBack == 's')
+        {
+            state = 3;
+        }
         break;
-    
+    case 3:                //Operatinal state
+        if(stepOrBack == 's')
+        {
+            state = 3;    //For now since we are only in part 1
+        }
+        else if(stepOrBack == 'b')
+        {
+            state = 1;
+        }
+        
+        break;
+        
     default:
-        break;
-    }
-    Serial.print("Current state: ");
-    Serial.println(state);
-}
-
-void stateMachine::back(){
-    switch (state)
-    {
-    case 2:
-        initilization();
-        state = 1;
-        break;
-
-    default:
+        Serial.print("Something went wrong state out of bounds");
         break;
     }
     Serial.print("Current state: ");
@@ -106,28 +96,25 @@ void stateMachine::stop(){
 }
 
 
-void stateMachine::readData(){
-    //Serial.read function
-    if(Serial.available() > 0){
-        serRead = Serial.read();
-    }
-}
-void stateMachine::writeData(){
-    //For pre operational
-    // Serial.write('Ready')
-}
-
 void stateMachine::restart(){
     //When writing 'r' or 'R' to console the program goes to state 'Initialization'
-    initilization();
+    // initilization();
 }
 
-void stateMachine::initilization(){
+void stateMachine::operationToDo(){
     //This is what initilization state is supposed to perform
+    switch (state)
+    {
+    case 1:             //Initilization
+        stateMachine();
+        break;
+    case 3:             //Operational
+        stateMachine(true);
+        break;
 
-    stateMachine(false);       
-}
-void stateMachine::operational(){
-    //This is what operational state is supposed to perform
-    stateMachine(true);
+    
+    default:
+        break;
+    }
+           
 }
--- Poject_3_CANopenSM/src/statemachine.h
+++ Poject_3_CANopenSM/src/statemachine.h
@@ -10,23 +10,22 @@ class stateMachine{
     public:
         stateMachine();
         stateMachine(bool LED);
-        void step();
-        void back();
+        void updateState(char stepOrBack);
         void stop();
-        void blink();
-        void readData();
-        void writeData();
         void restart();
-        void initilization();
-        void operational();
-         
+        
+        //Operations of each state
+        void operationToDo();
+        
 
     private:
-        int state;             //1 = Initilization, 2 = Operational 
+        int state;             //Initilization = 1, Operational = 3                   
         int analogReadValue;
+        int ledIntensity;
         bool ledUsed;
         char serRead;
-        Analog_in testInput;    //Analog input á A5 (5)
+        
+        Analog_in lightInput;    //Analog input á A5 (5)
         Digital_in buttonInput; //Button input on pin 12 (PB4) with ISR set on true. (0, false);
-        //PWM_out ledTimer;
+        PWM_out ledTimer;
 };
